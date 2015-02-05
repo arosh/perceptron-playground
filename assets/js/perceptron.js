e5cce50d24d5da21b6578d38351670de4a101196
Sho Iizuka <sho.i518@gmail.com>
2015-02-05 20:44:40 +0900

    scaleを実装した & ランダムに配置できるようにした

--- assets/js/perceptron.js
+++ assets/js/perceptron.js
@@ -1,16 +1,8 @@
-// 原点を通る超平面に限定したほうが良さそう…
-// scaleを合わせないとまともに学習できなさそう
 (function() {
   var width = 940, height = 540;
-  var svg = d3.select("#svgArea").attr({
+  var svg = d3.select("svg").attr({
     width: width, height: height, style: "background-color: whitesmoke;"
   });
-  svg.append("line").attr({
-    stroke: "tomato",
-    "stroke-width": 3,
-    x1: 0, y1: height/2, x2: width, y2: height/2
-  });
-
   var klass = 1;
   var colors = ["#51a351", "#f89406"];
   var datasets = [];
@@ -28,20 +20,18 @@
     }
   });
 
-  svg.on("click", function() {
-    var mode = null;
-    var position = d3.mouse(this);
+  var addDatum = function(klass, point) {
     datasets.push({
       klass: klass,
-      fv: [100.0, position[0], position[1]]
+      fv: [1.0, 1.0*point[0]/width, 1.0*point[1]/height]
     });
     svg.selectAll("circle").data(datasets).enter()
       .append("circle").attr({
         cx: function(d, i) {
-          return d.fv[1];
+          return d.fv[1]*width;
         },
         cy: function(d, i) {
-          return d.fv[2];
+          return d.fv[2]*height;
         },
         r: function(d, i) {
           return 15;
@@ -50,6 +40,11 @@
           return colors[d.klass - 1];
         }
       });
+  };
+
+  svg.on("click", function() {
+    var position = d3.mouse(this);
+    addDatum(klass, position);
   });
 
   var perceptron = {
@@ -72,15 +67,15 @@
     }
   };
 
-  d3.select("#button-clear").on("click", function() {
+  var clearDatasets = function() {
     // http://stackoverflow.com/questions/1232040/empty-an-array-in-javascript
     datasets = [];
     svg.selectAll("circle").remove();
     perceptron.clear();
-    svg.select("line").attr({
-      x1: 0, y1: height/2, x2: width, y2: height/2
-    });
-  });
+    svg.select("line").remove();
+  };
+
+  d3.select("#button-clear").on("click", clearDatasets);
 
   var learn = function() {
     if(datasets.length == 0) return;
@@ -97,7 +92,6 @@
 
   var getPoints = function() {
     var w = perceptron.weight.concat();
-    w[0] *= 100.0;
     if(w[1] == 0 && w[2] == 0) {
       return {
         x1: 0,
@@ -109,32 +103,37 @@
     if(w[1] == 0) {
       return {
         x1: 0,
-        y1: -w[0] / w[2],
-        x2: 0,
-        y2: -w[0] / w[2]
+        y1: -w[0] / w[2] * height,
+        x2: width,
+        y2: -w[0] / w[2] * height
       };
     }
     if(w[2] == 0) {
       return {
-        x1: -w[0] / w[1],
+        x1: -w[0] / w[1] * width,
         y1: 0,
-        x2: -w[0] / w[1],
-        y2: 0
+        x2: -w[0] / w[1] * width,
+        y2: height
       };
     }
     var y1 = -w[0]/w[2];
-    var y2 = -(w[1]*width + w[0])/w[2];
+    var y2 = -(w[1] + w[0])/w[2];
     return {
       x1: 0,
-      y1: y1,
+      y1: y1*height,
       x2: width,
-      y2: y2
+      y2: y2*height
     };
   };
 
   var render = function() {
     var p = getPoints();
+    if (svg.select("line").empty()) {
+      svg.append("line");
+    }
     svg.select("line").attr({
+      stroke: "tomato",
+      "stroke-width": 4,
       x1: p.x1, y1: p.y1, x2: p.x2, y2: p.y2
     });
   };
@@ -142,14 +141,51 @@
   d3.select("#button-gt").on("click", function() {
     learn();
     render();
-    console.log(perceptron.weight);
-    var ok = 0;
-    for(var i = 0; i < datasets.length; i++){
-      var fv = datasets[i].fv;
-      if(datasets[i].klass == 1 && perceptron.predict(fv) > 0) ok++;
-      if(datasets[i].klass == 2 && perceptron.predict(fv) < 0) ok++;
+  });
+
+  d3.select("#button-gtgt").on("click", function() {
+    for(var c = 0; c < datasets.length * 100; c++) {
+      learn();
+      render();
+      var flag = true
+      for(var i = 0; i < datasets.length; i++) {
+        if(datasets[i].klass == 1 && perceptron.predict(datasets[i].fv) < 0) {
+          flag = false;
+          break;
+        }
+        if(datasets[i].klass == 2 && perceptron.predict(datasets[i].fv) > 0) {
+          flag = false;
+          break;
+        }
+      }
+      if(flag) {
+        break;
+      }
     }
-    console.log("ok = " + ok);
+  });
+
+  d3.select("#button-random").on("click", function() {
+    clearDatasets();
+    var centerx = Math.floor(Math.random() * width);
+    var centery = Math.floor(Math.random() * height);
+    var theta = Math.random() * Math.PI;
+    var px = Math.cos(theta);
+    var py = Math.sin(theta);
+    for(var i = 0; i < 100; i++) {
+      var x = Math.floor(Math.random() * width);
+      var y = Math.floor(Math.random() * width);
+      var dx = x - centerx;
+      var dy = y - centery;
+      var sgn = px * dy - py * dx;
+      var k;
+      if(sgn >= 0) {
+        k = 1;
+      }
+      else {
+        k = 2;
+      }
+      addDatum(k, [x, y]);
+    }
   });
 
 })();
